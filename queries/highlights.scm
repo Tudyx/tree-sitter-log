65660fe9062daa83ce7a20d239b19fdc0da7425b
Tudyx <56633664+Tudyx@users.noreply.github.com>
2023-10-29 22:10:59 +0100

    update highlight

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,12 +1,15 @@
-(debug_level) @diff.plus
-(info_level) @info
-(warning_level) @warning
-(error_level) @error
-(log_date) @keyword
-(log_time) @constant
-; (comment) @comment
-(quoted_string) @string
+(debug) @diff.plus
+(info) @info
+(warn) @warning
+(error) @error
+(year_month_day) @keyword
+(date) @constant
+(string_literal) @string
+(number) @constant.builtin
 ; (uuid_gen) @constant.numeric
 ; (constant) @constant.numeric
-(constant) @constant.builtin
-(true_or_false) @constant.builtin
+; (constant) @constant.builtin
+; (true_or_false) @constant.builtin
+
+
+
